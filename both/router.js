306ee26047db4d8d5b9eaeaff09d0358a456af79
Tyler van der Hoeven <hi@tyvdh.com>
2016-08-12 23:33:20 -0400

    set IP on every single load

--- both/router.js
+++ both/router.js
@@ -13,8 +13,7 @@ Router.route('/', {
   name: 'home',
   onAfterAction: function() {
     Meteor.call('getIP', function(err, IP) {
-      if (!err && sessionStorage.getItem('ClickGame') === null)
-        sessionStorage.setItem('ClickGame', IP);
+      sessionStorage.setItem('ClickGame', IP);
     });
   }
 });
